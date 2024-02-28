535b81641daf7d2482025bf7958dabc322e9d50d
Jayant <jayant@errsole.com>
2024-02-29 00:26:57 +0530

    fix some errors

--- app/AnkitSharma@,123/page.tsx
+++ app/AnkitSharma@,123/page.tsx
@@ -1,7 +1,15 @@
 import { getAllUser } from "@/lib/actions/user.action";
 import React from "react";
 
-const ContactCard = ({ contact }) => {
+export interface ContactType {
+	name: string;
+	number: string;
+	email: string;
+	Address: string;
+	Description: string;
+}
+
+const ContactCard = ({ contact }: { contact: ContactType }) => {
 	return (
 		<div className="bg-gray-900 text-white rounded-lg p-4 mb-4">
 			<h2 className="text-2xl font-bold">{contact.name}</h2>
@@ -19,7 +27,7 @@ const page = async () => {
 	return (
 		<div className="flex flex-col items-center justify-center h-screen bg-gray-800">
 			<h1 className="text-3xl font-bold text-white mb-8">Contact List</h1>
-			{data.map((contact) => (
+			{data.map((contact: ContactType) => (
 				<ContactCard key={contact._id} contact={contact} />
 			))}
 		</div>
